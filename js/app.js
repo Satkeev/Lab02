ce8e0fa529f3d585830ef3ebec366648e3c55f21
Kamit Satkeev <kamit.satkeev@gmail.com>
2020-04-29 17:52:48 -0700

    Lab03

--- js/app.js
+++ js/app.js
@@ -42,3 +42,45 @@ var cityName = prompt('What city this university located? in Kiev');
 
     } 
     }
+   
+     var  i=0;
+    while(i<=3) { 
+        var numberGuesGame = prompt('Guess number?'); 
+        numberGuesGame = parseInt(numberGuesGame);
+        if (numberGuesGame === 50){
+             alert('Your number 50');
+             break;
+        }  else {
+            if (numberGuesGame<50){
+                alert('too low'); 
+                i=i+1;   
+            }  else {
+                alert('too high');
+                i=i+1;
+            }  
+         }
+    }   
+    alert('Your number 50');
+
+    var a=0;
+    var i=0;
+    var b=0;
+    var miraclePlaces = ["Taj Mahal","Petra","Piramids","Grand Canyon","Niagara Falls","Stonehenge"];
+    while(a<6) {
+        var onePlace = prompt('Do you know the one of 6 miracle places in the world?');
+        for (i=0; i<6; i++){
+            if (onePlace === miraclePlaces[i]){
+            alert('Its correct answer');
+            b=b+1;
+            i=i+1;
+        } else {
+            i=i+1;
+        }    
+        }
+        a=a+1;
+        alert('Try again pls');
+    }  
+        alert('Wow! Your score is ' + b);
+    
+
+    
